814ecb4d5fd64545d38b4f5791fa767b7189ffc3
Marc Vilchez <ht.m4rck@gmail.com>
2026-05-07 18:49:58 -0500

    Railway rutas v006

--- apps/front/docker-entrypoint.sh
+++ apps/front/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${PORT} ${API_UPSTREAM}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
